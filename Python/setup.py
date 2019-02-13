ca72db8a6cf03598b5ef4d71d6b24f970d9b52f0
Henryk T. Haniewicz <henryk.haniewicz@gmail.com>
2019-02-13 10:51:49 +0000

    Refactored everything for packaging

---
+++ Python/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open( "README.md", "r" ) as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MathFunctions - Python",
+    version="0.0.1",
+    author="Henryk Haniewicz",
+    author_email="henryk.haniewicz@gmail.com",
+    description="A small math package for my own amusement",
+    long_description=long_description,
+    url="https://github.com/HenrykHaniewicz/MathFunctions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
